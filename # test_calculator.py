28fc677f040cba4b6f983bbaa979516ca6893182
Sara Talaat <S.Ahmed2244@nu.edu.eg>
2024-11-15 15:22:38 +0200

    Delete # test_calculator.py

--- # test_calculator.py
+++
@@ -1,8 +0,0 @@
-# test_calculator.py
-import unittest
-from calculator import Calculator
-
-class TestCalculator(unittest.TestCase):
-    def test_add(self):
-        calc = Calculator()
-        self.assertEqual(calc.add(2, 3), 5)  
